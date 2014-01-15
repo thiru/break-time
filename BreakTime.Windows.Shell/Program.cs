a13a9efd52b3b3a7f05705f497dce002c2fa4a83
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-01-14 23:32:20 -0500

    System tray icon minimally functional

--- BreakTime.Windows.Shell/Program.cs
+++ BreakTime.Windows.Shell/Program.cs
@@ -2,13 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace BreakTime.Windows.Shell
 {
-    class Program
+  class Program
+  {
+    /// <summary>
+    /// Application entry point.
+    /// </summary>
+    /// <param name="args">Command-line arguments.</param>
+    [STAThread]
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+      var trayIcon = new TrayIcon();
+      trayIcon.Show();
+      Application.Run();
     }
+  }
 }
---
+++ BreakTime.Windows.Shell/TrayIcon.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace BreakTime.Windows.Shell
+{
+  /// <summary>
+  /// Encapsulates the system tray icon.
+  /// </summary>
+  public class TrayIcon : IDisposable
+  {
+    #region Init
+    private readonly NotifyIcon _notifyIcon;
+    private bool _isDisposed;
+
+    /// <summary>
+    /// Initialises a new instance of this class.
+    /// </summary>
+    public TrayIcon()
+    {
+      _notifyIcon = CreateNotifyIcon();
+    }
+
+    /// <summary>
+    /// Class destruction and cleanup.
+    /// </summary>
+    ~TrayIcon()
+    {
+      Dispose(false);
+    }
+    #endregion Init ----------------------------------------------------------------------------------------------
+
+    #region Public Methods
+    /// <summary>
+    /// Show the tray icon.
+    /// </summary>
+    public void Show()
+    {
+      if (_isDisposed) throw new ObjectDisposedException("_notifyIcon");
+      _notifyIcon.Visible = true;
+    }
+
+    /// <summary>
+    /// Show a brief informational, balloon tip with the specified message.
+    /// </summary>
+    public void ShowBalloonTip(string message)
+    {
+      _notifyIcon.ShowBalloonTip(0, "Break Time", message, ToolTipIcon.Info);
+    }
+
+    /// <summary>
+    /// Dispose of underlying <see cref="NotifyIcon"/>.
+    /// </summary>
+    public void Dispose()
+    {
+      Dispose(true);
+      GC.SuppressFinalize(this);
+    }
+    #endregion Public Methods ------------------------------------------------------------------------------------
+
+    #region Non-Public Methods
+    /// <summary>
+    /// Dispose of underlying <see cref="NotifyIcon"/>.
+    /// </summary>
+    protected virtual void Dispose(bool isDisposing)
+    {
+      if (!_isDisposed)
+      {
+        if (isDisposing)
+          _notifyIcon.Dispose();
+
+        _isDisposed = true;
+      }
+    }
+
+    private NotifyIcon CreateNotifyIcon()
+    {
+      var notifyIcon = new NotifyIcon
+      {
+        ContextMenu = new ContextMenu
+        (
+          new[]
+          {
+            new MenuItem("Settings", delegate { MessageBox.Show("TODO"); }),
+            new MenuItem("Log", delegate { MessageBox.Show("TODO"); }),
+            new MenuItem("About", delegate { MessageBox.Show("TODO"); }),
+            new MenuItem("-"),
+            new MenuItem("Reset", delegate { MessageBox.Show("TODO"); }),
+            new MenuItem("-"),
+            new MenuItem("Exit", delegate { Dispose(); Application.Exit(); })
+          }
+        ),
+        Icon = new Icon(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "flower.ico")),
+        Text = "Break Time",
+      };
+
+      notifyIcon.DoubleClick += delegate { MessageBox.Show("TODO"); };
+
+      return notifyIcon;
+    }
+    #endregion Non-Public Methods --------------------------------------------------------------------------------
+  }
+}
