e4811f32475637420046a72d35b8ddcac5e8c0a1
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-01-14 22:31:22 -0500

    Empty Windows console (shell) project added
    This project is the shell for the Windows-specific version of Break
    Time.  It will handle the application life-cycle, Windows-specifc
    events, and launch the OS-independent UI layer.

---
+++ BreakTime.Windows.Shell/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Break Time")]
+[assembly: AssemblyDescription("Break Time for Windows Desktop.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Thirushanth Thirunavukarasu")]
+[assembly: AssemblyProduct("Break Time")]
+[assembly: AssemblyCopyright("Copyright © Thirushant Thirunavukarasu 2012-2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c66eafc-d3bd-4659-b0d3-b09e7d7a3d33")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.0")]
+[assembly: AssemblyFileVersion("3.0")]
