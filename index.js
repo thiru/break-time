df9d706627b6b9423c3ecd85c6ff22d41981d111
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-01-26 21:27:48 -0500

    Included open project for opening web pages

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+var path = require('path');
+var open = require('open');
+
 console.log('App started.');
+
+var homePage = path.join(process.cwd(), '/ui/index.html');
+open('file://' + homePage);
+
 console.log('App exited.');
