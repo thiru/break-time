932b6a71993aec7a155cd3dd18d22bece593cbd2
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-01-27 20:12:13 -0500

    Basic static web server using Connect

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var path = require('path');
-var open = require('open');
-
 console.log('App started.');
 
-var homePage = path.join(process.cwd(), '/ui/index.html');
-open('file://' + homePage);
+var open = require('open');
+var connect = require('connect');
 
-console.log('App exited.');
+var port = 8080;
+var app = connect()
+            .use(connect.logger('dev'))
+            .use(connect.favicon(__dirname + '/ui/images/favicon.ico'))
+            .use(connect.static('ui'))
+            .use(connect.errorHandler()) // Only use for dev/testing
+            .listen(port);
+
+open('http://localhost:' + port);
