400725141a6ddc4b08802acd6ba3a922c6088806
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-01-29 16:08:00 -0500

    Skeletal Express app

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
-console.log('App started.');
+console.log('Starting Break Time...');
 
+process.stdout.write('Loading dependencies... ');
+var express = require('express');
+var http = require('http');
+var path = require('path');
 var open = require('open');
-var connect = require('connect');
+console.log('done.');
 
-var port = 8080;
-var app = connect()
-            .use(connect.logger('dev'))
-            .use(connect.favicon(__dirname + '/ui/images/favicon.ico'))
-            .use(connect.static('ui'))
-            .use(connect.errorHandler()) // Only use for dev/testing
-            .listen(port);
+process.stdout.write('Configuring Express... ');
+var defaultPort = 8080;
+var app = express();
 
-open('http://localhost:' + port);
+app.set('port', process.env.PORT || defaultPort);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.logger('dev'));
+app.use(express.favicon(__dirname + '/ui/images/favicon.ico'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static('ui'));
+
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+console.log('done.');
+
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Break Time\'s web server started and listening on port ' + app.get('port') + '.');
+  process.stdout.write('Opening home page... ');
+  open('http://localhost:' + app.get('port'));
+  console.log('done');
+});
+
