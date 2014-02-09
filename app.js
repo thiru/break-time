c5a73b5932bd84d224e4c2d9518123f385f0fe4a
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-02-09 14:27:52 -0500

    Refactoring

--- app.js
+++ app.js
@@ -5,34 +5,47 @@ var path = require('path');
 var open = require('open');
 var routes = require('./routes');
 
-console.log('[START] '.yellow + 'Break Time start-up');
-console.log('[START] '.yellow +  'Express configuration');
 var defaultPort = 8080;
-var app = express();
 
-app.set('port', process.env.PORT || defaultPort);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.logger('dev'));
-app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static('public'));
+console.log('[START] '.yellow + 'Break Time start-up');
 
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+var app = initExpress();
+http.createServer(app).listen(app.get('port'), onServerStarted);
+
+// Configure and initialise Express.
+function initExpress()
+{
+  console.log('[START] '.yellow +  'Express configuration');
+  var app = express();
+  app.set('title', 'Break Time');
+  app.set('port', process.env.PORT || defaultPort);
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'jade');
+  app.use(express.logger('dev'));
+  app.use(express.favicon(path.join(__dirname, '/public/images/favicon.ico')));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static('public'));
+
+  if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+  }
+
+  // Routes
+  app.get('/', routes.index);
+
+  console.log('[END] '.green + 'Express configuration');
+  return app;
 }
 
-app.set('title', 'Break Time');
-app.get('/', routes.index);
-console.log('[END] '.green + 'Express configuration');
-
-http.createServer(app).listen(app.get('port'), function() {
-  console.log('Express server started and listening on port ' + app.get('port') + '.');
+// Open home page when server has started.
+function onServerStarted()
+{
+  console.log('Web server started and listening on port ' + app.get('port') + '.');
   console.log('[START] '.yellow + 'Open home page');
   open('http://localhost:' + app.get('port'));
   console.log('[END] '.green + 'Open home page');
   console.log('[END] '.green + 'Break Time start-up');
-});
+}
