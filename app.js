0f3a9dd88579b017fa2957ef505b974eb02c7567
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-02-09 14:10:14 -0500

    EJS templating replaced with Jade

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-console.log('Starting Break Time...');
-
 var colors = require('colors');
 var express = require('express');
 var http = require('http');
@@ -7,13 +5,14 @@ var path = require('path');
 var open = require('open');
 var routes = require('./routes');
 
+console.log('[START] '.yellow + 'Break Time start-up');
 console.log('[START] '.yellow +  'Express configuration');
 var defaultPort = 8080;
 var app = express();
 
 app.set('port', process.env.PORT || defaultPort);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'jade');
 app.use(express.logger('dev'));
 app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
 app.use(express.json());
@@ -32,7 +31,8 @@ console.log('[END] '.green + 'Express configuration');
 
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server started and listening on port ' + app.get('port') + '.');
-  console.log('[END] '.yellow + 'Open home page');
+  console.log('[START] '.yellow + 'Open home page');
   open('http://localhost:' + app.get('port'));
-  console.log('[START] '.green + 'Open home page');
+  console.log('[END] '.green + 'Open home page');
+  console.log('[END] '.green + 'Break Time start-up');
 });
