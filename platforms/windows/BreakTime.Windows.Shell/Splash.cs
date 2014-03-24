9441901e408d6cf4ec2bb2ecf47c40ba9a2d401a
Thiru Thirunavukarasu <thiru0130@gmail.com>
2014-03-23 22:16:24 -0400

    Windows shell project started
    So far this .NET 4 WinForms project just launches the splash screen and
    hides it.

---
+++ platforms/windows/BreakTime.Windows.Shell/Splash.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BreakTime.Windows.Shell
+{
+    /// <summary>
+    /// The splash screen.
+    /// </summary>
+    public partial class Splash : Form
+    {
+        /// <summary>
+        /// Initialises a new instance of this class.
+        /// </summary>
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            ClientSize = BackgroundImage.Size;
+            new Thread(HideWindow).Start();
+
+            base.OnLoad(e);
+        }
+
+        private void HideWindow()
+        {
+            Thread.Sleep(1000);
+            Invoke((Action)Hide);
+        }
+    }
+}
